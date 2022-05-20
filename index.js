be6ceb80eb3a3f980ee05b0d1664cc33653ce95e
Minhaj-Rabby <minhaj12113@gmail.com>
2022-05-20 12:33:16 +0600

    Ggetting all items

--- index.js
+++ index.js
@@ -31,3 +31,52 @@ const client = new MongoClient(uri, {
     useUnifiedTopology: true,
     serverApi: ServerApiVersion.v1,
 });
+
+async function run() {
+    try {
+        await client.connect();
+        const groseryCollection = client
+            .db("foodExpress")
+            .collection("groseryItems");
+        const sellCollection = client
+            .db("foodExpress")
+            .collection("sellProducts");
+        const soldCollection = client
+            .db("foodExpress")
+            .collection("soldProductsUniq");
+        
+        app.post("/login", async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(
+                user,
+                process.env.ACCESS_TOKEN_SECRET,
+                {
+                    expiresIn: "1d",
+                }
+            );
+
+            res.send({ accessToken });
+        });
+
+        //get all items from database
+        app.get("/products", async (req, res) => {
+            const size = parseInt(req.query.size);
+
+            const query = {};
+            const cursor = groseryCollection.find(query);
+            let products;
+            if (size) {
+                products = await cursor.limit(size).toArray();
+            } else {
+                products = await cursor.toArray();
+            }
+            res.send(products);
+        });
+
+    } finally {
+
+    }
+}
+
+run().catch(console.dir);
+
